f81a9e6677fe7894af4afe6a1f9cd59ccc0ba132
smaffy <jekrudcz@gmail.com>
2020-11-30 00:15:32 +0200

    1.1 useState

---
+++ readme.txt
@@ -0,0 +1,10 @@
+//was: this.setState({a: 1}) -> this.render()
+
+hooks = isolated funcs
+
+import React, {useState} from 'react';
+
+1. useState
+Array [ 0, dispatchAction() ]
+
+
--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 function App() {
-  return (
-    <div>
+    const [counter, setCounter] = useState(0)
 
-    </div>
-  );
+    function increment() {
+        setCounter(counter + 1)
+    }
+
+    function decrement() {
+        setCounter(counter - 1)
+    }
+
+    return (
+        <div className="wrapper">
+            <div>
+                <h1>Counter: {counter}</h1>
+                <button onClick={increment} className="btn btn-success">Add</button>
+                <button onClick={decrement} className="btn btn-danger">Remove</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
