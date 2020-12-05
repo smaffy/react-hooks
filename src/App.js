cadbb8b08c4a877acc9d4803e9c8c074f433b5b7
smaffy <jekrudcz@gmail.com>
2020-12-05 11:04:20 +0200

    3.1 useRef. focus

--- src/App.js
+++ src/App.js
@@ -7,21 +7,18 @@ function App() {
     const renderCount = useRef(1)
     const inputRef = useRef(null)
 
-    // endless cycle
-    // useEffect(() => {
-    //     setRenderCount(prev => prev + 1)
-    // })
-
+    const focus = () => inputRef.current.focus()
 
     useEffect(() => {
         renderCount.current++
-        console.log(inputRef.current.value)
+        // console.log(inputRef.current.value)
     })
 
     return (
         <div className='wrapper'>
             <h1>Render Count: {renderCount.current}</h1>
             <input ref={inputRef} type="text" onChange={e => setValue(e.target.value)} value={value} />
+            <button className="btn btn-success" onClick={focus} >Focus</button>
         </div>
     )
 }
