56aee4401d69fa27fc86c09ca5fc006dc0faa12e
smaffy <jekrudcz@gmail.com>
2020-12-05 15:32:39 +0200

    8.0.0 Custom Hooks

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import React, {useState} from 'react';
 
 function App() {
     const [name, setName] = useState('')
+    const [lastName, setLastName] = useState('')
 
     const changeHandler = event => {
         setName(event.target.value)
     }
 
+    const changeLastNameHandler = event => {
+        setLastName(event.target.value)
+    }
+
     return (
         <div className={'container pt-3'}>
-
-
             <input type="text" value={name} onChange={changeHandler} />
+            <input type="text" value={lastName} onChange={changeLastNameHandler} />
+            <hr />
+            <h1>{name} {lastName}</h1>
         </div>
     )
 }
